7d57af6ecebd5a612ccfa7c5570c99b74f9460b7
Kimberly Placha <Kimberly.Placha@sdgku.edu>
2024-03-08 09:47:27 -0800

    final

--- 106/scripts/app.js
+++ 106/scripts/app.js
@@ -10,24 +10,56 @@ function saveTask(){
     console.log(Title,Description,startDate,Budget,Status,Color);
 
     //build the object
-    let task =  new Task(title,description,startDate,budget,status,color);
-    console.log(task);
+    let x =  new Task(title,description,startDate,budget,status,color);
+    console.log(x);
 
 
+function testRequest(){
+    $.ajax({
+        type: "GET",
+        url:"http:fsdiapi.azurewebsites.net/",
+        success: function(response){
+        },
+        error: function(error)
+        {
+                console.log(error);
+        }
+    });
+}
 
     //save to server
+
+    $.ajax({
+        type: "POST",
+        url: "http://fsdiapi.azurewebsites.net/api/tasks",
+        data: JSON.stringify(task),
+        contentType: "application/json",
+        success: function(response){
+            console.log(response);
+        },
+        error: function(error)
+        {
+            console.log(error);
+        }
+    });
 
     //display the task
 }
 
+function testRequest(){
+    $.ajax({
+            type: "Get",
+            url: "https://fsdiapi.azurewebsites.net/"
+    })
+}
+
 function init(){
     console.log("this is a task manager");
-    //load data
+//load data
 
-
-   // hook events
+// hook events
     $("#btnSave").click(saveTask);
-   //document.getElementByID("btnSave")
+//document.getElementByID("btnSave")
 
 }
 
